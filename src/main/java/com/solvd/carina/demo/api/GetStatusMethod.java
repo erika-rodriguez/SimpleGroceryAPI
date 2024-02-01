0d3d3bcce074f6196b6bc94c7ab55b6894148d2c
Erika Rodriguez <rodriguez.eri92@gmail.com>
2024-02-01 15:37:35 -0300

    Get methods Grocery API

---
+++ src/main/java/com/solvd/carina/demo/api/GetStatusMethod.java
@@ -0,0 +1,18 @@
+package com.solvd.carina.demo.api;
+
+import com.zebrunner.carina.api.AbstractApiMethodV2;
+import com.zebrunner.carina.api.annotation.Endpoint;
+import com.zebrunner.carina.api.annotation.SuccessfulHttpStatus;
+import com.zebrunner.carina.api.http.HttpMethodType;
+import com.zebrunner.carina.api.http.HttpResponseStatusType;
+
+@Endpoint(url = "${config.env.api_url}/status", methodType = HttpMethodType.GET)
+@SuccessfulHttpStatus(status= HttpResponseStatusType.OK_200)
+public class GetStatusMethod extends AbstractApiMethodV2 {
+
+    public void getStatusOfAPI(){
+
+    }
+
+
+}
---
+++ src/test/java/com/solvd/carina/demo/GroceryAPITest.java
@@ -0,0 +1,30 @@
+package com.solvd.carina.demo;
+
+import com.solvd.carina.demo.api.GetAllProductsMethod;
+import com.solvd.carina.demo.api.GetStatusMethod;
+import com.zebrunner.carina.core.IAbstractTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+
+import java.lang.invoke.MethodHandles;
+
+public class GroceryAPITest implements IAbstractTest {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @Test
+    public void testAPIStatus(){
+        GetStatusMethod status=new GetStatusMethod();
+        status.getStatusOfAPI();
+        status.callAPIExpectSuccess();
+        status.validateResponseAgainstSchema("api/users/_get/rs.schema");
+    }
+
+    @Test
+    public void testGetAllProducts(){
+        GetAllProductsMethod products=new GetAllProductsMethod();
+        products.getProducts();
+        products.callAPI();
+        products.validateResponseAgainstSchema("api/users/_get/rs.schema");
+    }
+}
